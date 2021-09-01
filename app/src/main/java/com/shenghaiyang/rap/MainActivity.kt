f3140a3b783f65e1d863329250af68e681b81939
shenghaiyang <shenghaiyang@live.cn>
2021-09-02 00:49:13 +0800

    add dynamic register method

--- app/src/main/java/com/shenghaiyang/rap/MainActivity.kt
+++ app/src/main/java/com/shenghaiyang/rap/MainActivity.kt
@@ -1,20 +1,24 @@
 package com.shenghaiyang.rap
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val textView = findViewById<TextView>(R.id.text)
-        textView.text = stringFromJNI("Rust")
+        textView.text = "${stringFromJNI("rust")}\n${dynamicRegisterMethod("rust")}"
     }
 
     private external fun stringFromJNI(input: String): String
+
+    private external fun dynamicRegisterMethod(input: String): String
 
     companion object {
         init {
--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,8 +1,11 @@
 extern crate jni;
 
-use jni::JNIEnv;
+use std::ffi::c_void;
+
+use jni::{JavaVM, JNIEnv, NativeMethod};
 use jni::objects::{JClass, JString};
-use jni::sys::jstring;
+use jni::strings::JNIString;
+use jni::sys::{jint, JNI_ERR, jstring};
 
 #[no_mangle]
 #[allow(non_snake_case)]
@@ -20,4 +23,45 @@ pub extern "C" fn Java_com_shenghaiyang_rap_MainActivity_stringFromJNI(
         .expect("Couldn't create java string!");
 
     out.into_inner()
+}
+
+#[no_mangle]
+pub fn dynamic_register_method(
+    env: JNIEnv,
+    _class: JClass,
+    input: JString,
+) -> jstring {
+    let input: String = env
+        .get_string(input)
+        .expect("Couldn't get java string!")
+        .into();
+    let out = env
+        .new_string(format!("Hello, dynamic {}!", input))
+        .expect("Couldn't create java string!");
+
+    out.into_inner()
+}
+
+#[no_mangle]
+#[allow(non_snake_case)]
+pub unsafe extern fn JNI_OnLoad(vm: JavaVM, _reserved: *mut c_void) -> jint {
+    let env = vm.get_env().unwrap();
+    let version = env.get_version().unwrap().into();
+
+    let class = env.find_class("com/shenghaiyang/rap/MainActivity").unwrap();
+
+    let methods = [
+        NativeMethod {
+            name: JNIString::from("dynamicRegisterMethod"),
+            sig: JNIString::from("(Ljava/lang/String;)Ljava/lang/String;"),
+            fn_ptr: dynamic_register_method as *mut c_void,
+        }
+    ];
+
+    let result = env.register_native_methods(class, &methods);
+    if result.is_ok() {
+        version
+    } else {
+        JNI_ERR
+    }
 }
